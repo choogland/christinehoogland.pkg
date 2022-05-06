187530cc1bcf3edc7f600da8d6623cb543ef2350
choogland <christine.hoogland@finddx.org>
2022-05-06 10:08:52 +0000

    Implement bmi function

---
+++ R/data_weight.R
@@ -0,0 +1,12 @@
+#' Population physical data
+#'
+#' A dataset containing mass and height for some characters.
+#'
+#' @format A data frame with 87 rows and 3 variables:
+#' \describe{
+#'   \item{name}{name of the character}
+#'   \item{mass}{mass in kg}
+#'   \item{height}{height in m}
+#'   ...
+#' }
+"data_weight"
